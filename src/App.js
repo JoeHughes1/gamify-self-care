f2b91025148a329b516466a8f03f795ebe9d078d
Joe <joehughes196@gmail.com>
2023-09-09 13:14:21 -0700

    added ability to add custom tasks. only name is customizable for now

--- src/App.js
+++ src/App.js
@@ -2,33 +2,45 @@ import { useState } from "react";
 import React from "react";
 
 export default function App() {
-	return (
-		<>
-			<h1>Gamify Self Care</h1>
-			<div className="task-list">
-				<TaskList />
-			</div>
-		</>
-	);
-}
-
-function TaskList() {
 	const [tasks, setTasks] = useState([]);
+	const [taskName, setTaskName] = useState("");
 
-	function handleSubmit() {
-		setTasks((task) => [
+	function handleSubmitCustom(e) {
+		e.preventDefault();
+
+		setTasks([
 			...tasks,
 			{
-				name: task.name,
-				points: task.points,
+				name: taskName,
+				points: 0,
 				id: Date.now(),
 				complete: false,
 			},
 		]);
 
+		setTaskName("");
+
 		console.log(tasks);
 	}
 
+	return (
+		<>
+			<h1>Gamify Self Care</h1>
+			<div className="task-list">
+				<TaskList tasks={tasks} />
+			</div>
+			<div className="custom-add-task__container">
+				<CustomAddTask
+					onSubmit={handleSubmitCustom}
+					taskName={taskName}
+					setTaskName={setTaskName}
+				/>
+			</div>
+		</>
+	);
+}
+
+function TaskList({ tasks }) {
 	const initialTasks = [
 		{
 			id: 1,
@@ -48,12 +60,15 @@ function TaskList() {
 		},
 	];
 
-	const taskList = initialTasks.map((task) => <Task task={task} />);
+	const taskList = tasks.map((task) => (
+		<li key={task.id}>
+			<Task task={task} />
+		</li>
+	));
 
 	return (
 		<div>
 			<h2>Today's Tasks</h2>
-			<CustomAddTask onSubmit={handleSubmit} />
 			{taskList}
 		</div>
 	);
@@ -61,7 +76,7 @@ function TaskList() {
 
 function Task({ task }) {
 	return (
-		<div
+		<ul
 			key={task.id}
 			className={
 				task.complete ? "task-list__task complete" : "task-list__task"
@@ -69,25 +84,45 @@ function Task({ task }) {
 		>
 			<h5>{task.name}</h5>
 			<p>{task.points} pts</p>
-			<input type="checkbox"></input>
-		</div>
+			<input type="checkbox" className="task-list__task-checkbox"></input>
+		</ul>
 	);
 }
 
-function CustomAddTask({ onSubmit }) {
+function CustomAddTask({ onSubmit, taskName, setTaskName }) {
+	const repeat = true;
+
 	return (
-		<form className="custom-task-form" onSubmit={onSubmit}>
+		<form onSubmit={onSubmit} className="custom-task-form">
+			<h2>ADD CUSTOM TASK</h2>
 			<input
 				type="text"
 				placeholder="Add new task..."
 				className="custom-task-form__name"
+				value={taskName}
+				onChange={(e) => setTaskName(e.target.value)}
 			></input>
 			<input
 				type="number"
 				placeholder="points"
 				className="custom-task-form__pts"
 			></input>
-			<button>+</button>
+
+			<span>
+				<p>
+					Repeat?<input type="checkbox"></input>
+				</p>
+				{repeat ? (
+					<select className="custom-task-form__repeat">
+						<option value="daily">daily</option>
+						<option value="weekly">weekly</option>
+						<option value="monthly">monthly</option>
+					</select>
+				) : (
+					""
+				)}
+			</span>
+			<button className="custom-task-form__submit">ADD</button>
 		</form>
 	);
 }
