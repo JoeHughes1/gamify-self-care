0b6f5675ed5ae47f59cef9e22ccc567cbb043151
Joe <joehughes196@gmail.com>
2023-09-09 01:09:04 -0700

    added some styling. started to add custom task form and logic

--- src/App.js
+++ src/App.js
@@ -2,45 +2,92 @@ import { useState } from "react";
 import React from "react";
 
 export default function App() {
-	function TaskList() {
-		const initialTasks = [
-			{
-				id: 1,
-				name: "brush teeth",
-				points: 10,
-				repeats: true,
-				cycle: 0.5,
-			},
-			{
-				id: 2,
-				name: "wash face",
-				points: 10,
-				repeats: true,
-				cycle: 0.5,
-			},
-		];
-
-		return initialTasks.map((task) => (
+	return (
+		<>
+			<h1>Gamify Self Care</h1>
 			<div className="task-list">
-				<Task name={task.name} key={task.id} />
+				<TaskList />
 			</div>
-		));
+		</>
+	);
+}
+
+function TaskList() {
+	const [tasks, setTasks] = useState([]);
+
+	function handleSubmit() {
+		setTasks((task) => [
+			...tasks,
+			{
+				name: task.name,
+				points: task.points,
+				id: Date.now(),
+				complete: false,
+			},
+		]);
+
+		console.log(tasks);
 	}
 
-	function Task({ name, key }) {
-		return (
-			<div key={key} className="task-list__task">
-				<p>task name</p>
-				<input type="checkbox"></input>
-				<p>points</p>
-			</div>
-		);
-	}
+	const initialTasks = [
+		{
+			id: 1,
+			name: "brush teeth",
+			points: 10,
+			repeats: true,
+			cycle: 0.5,
+			complete: false,
+		},
+		{
+			id: 2,
+			name: "wash face",
+			points: 10,
+			repeats: true,
+			cycle: 0.5,
+			complete: false,
+		},
+	];
+
+	const taskList = initialTasks.map((task) => <Task task={task} />);
 
 	return (
 		<div>
-			<h1>Gamify Self Care</h1>
-			<TaskList />
+			<h2>Today's Tasks</h2>
+			<CustomAddTask onSubmit={handleSubmit} />
+			{taskList}
 		</div>
+	);
+}
+
+function Task({ task }) {
+	return (
+		<div
+			key={task.id}
+			className={
+				task.complete ? "task-list__task complete" : "task-list__task"
+			}
+		>
+			<h5>{task.name}</h5>
+			<p>{task.points} pts</p>
+			<input type="checkbox"></input>
+		</div>
+	);
+}
+
+function CustomAddTask({ onSubmit }) {
+	return (
+		<form className="custom-task-form" onSubmit={onSubmit}>
+			<input
+				type="text"
+				placeholder="Add new task..."
+				className="custom-task-form__name"
+			></input>
+			<input
+				type="number"
+				placeholder="points"
+				className="custom-task-form__pts"
+			></input>
+			<button>+</button>
+		</form>
 	);
 }
